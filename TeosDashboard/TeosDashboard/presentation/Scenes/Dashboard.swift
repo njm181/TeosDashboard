6cc216dd8f8ca0c73a3e24f4a9fe220b8f465b1e
Nicolas Molina <nicolasjmolina1@gmail.com>
2025-12-08 21:41:48 -0300

    Second commit

---
+++ TeosDashboard/TeosDashboard/presentation/Scenes/Dashboard.swift
@@ -0,0 +1,18 @@
+//
+//  Dashboard.swift
+//  TeosDashboard
+//
+//  Created by Nico Molina on 08/12/2025.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dashboard()
+}
