4e886588b1a3d90d7d203ac661dce477c3988998
Nicolas Molina <nicolasjmolina1@gmail.com>
2025-12-08 21:35:47 -0300

    First commit

---
+++ TeosDashboard/TeosDashboard/TeosDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeosDashboardApp.swift
+//  TeosDashboard
+//
+//  Created by Nico Molina on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TeosDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeosDashboard/TeosDashboardTests/TeosDashboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeosDashboardTests.swift
+//  TeosDashboardTests
+//
+//  Created by Nico Molina on 08/12/2025.
+//
+
+import Testing
+@testable import TeosDashboard
+
+struct TeosDashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
