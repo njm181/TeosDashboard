98e80c8410479ff5ac9d392dfdef3418019be386
Nicolas Molina <nicolasjmolina1@gmail.com>
2025-12-08 21:45:05 -0300

    Third commit

--- TeosDashboard/TeosDashboard/presentation/Scenes/DashboardView.swift
+++ TeosDashboard/TeosDashboard/presentation/Scenes/DashboardView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Dashboard: View {
+struct DashboardView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Dashboard()
+    DashboardView()
 }
