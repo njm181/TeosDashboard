75b7bba2c458ca44bc7fc6cdfaf69ef9e79d54c8
Nicolas Molina <nicolasjmolina1@gmail.com>
2025-12-08 21:45:27 -0300

    Third commit

--- TeosDashboard/TeosDashboard/TeosDashboardApp.swift
+++ TeosDashboard/TeosDashboard/TeosDashboardApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TeosDashboardApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // ContentView()
+            DashboardView()
         }
     }
 }
